8d45a4b81339030f0fd32bd5ca1cdfe95d15f6d4
yena <2371429@hansung.ac.kr>
2025-07-24 23:12:22 +0900

    ✨ : RightPracticeBox 컴포넌트 생성

---
+++ src/screens/practiceScreen/components/RightPracticeBox.jsx
@@ -0,0 +1,64 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { COLORS } from "../../../styles/color"
+import { useState } from "react"
+
+export const RightPracticeBox = () => {
+    const [selected, setSelected] = useState(null);
+
+    const [sentence, setSentence] = useState([
+        "한 명이에요", "두 명이에요", "잠시만요"
+    ])
+
+    return (
+        <View style = { styles.container }>
+            { sentence.map((text, index) => (
+                <TouchableOpacity  
+                    key = { index }
+                    style = {[ 
+                        styles.choiceContainer,
+                        selected === index && styles.selectContainer 
+                    ]}
+                    onPress = { () => {
+                        if ( selected === index ) setSelected(null);
+                        else setSelected(index) }
+                    }
+                >
+                    <Text style = { styles.text }>{ text }</Text>
+                </TouchableOpacity>                
+            ))}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 214,
+        height: 88,
+        borderRadius: 20,
+        backgroundColor: COLORS.MAIN_YELLOW1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 6
+    },
+
+    choiceContainer: {
+        width: 200,
+        height: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: COLORS.BACKGROUND,
+        borderRadius: 10,
+        paddingVertical: 4,
+        paddingHorizontal: 10
+    },
+
+    selectContainer: {
+        backgroundColor: "#FFD32199"
+    },
+
+    text: {
+        fontSize: 10,
+        fontWeight: 400,
+        lineHeight: 11
+    }
+})
