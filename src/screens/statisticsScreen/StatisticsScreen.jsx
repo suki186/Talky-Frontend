2a0319851360ffffa6450c91906e4f99a5f15d8b
Suki <tues1205@naver.com>
2025-08-01 01:02:03 +0900

    ✨ : 통계화면 연결된계정 Selector

--- src/screens/statisticsScreen/StatisticsScreen.jsx
+++ src/screens/statisticsScreen/StatisticsScreen.jsx
@@ -1,12 +1,41 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import Selector from "../../components/Selector";
 
 const StatisticsScreen = () => {
+  const users = ["김예나", "조주한"]; // 연결된 계정 목록
+
+  const [user, setUser] = useState(users[0]);
   return (
     <View>
-      <Text>StatisticsScreen</Text>
+      <View style={styles.userList}>
+        {/* 연결된 일반 사용자 목록 */}
+        <Selector
+          items={users}
+          width="94"
+          selectedValue={user}
+          onSelect={setUser}
+          variant="user"
+        />
+        <Text style={styles.infoText}>
+          최근 7일 동안의 활동 데이터를 기반으로한 결과입니다.
+        </Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  userList: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    gap: 8,
+  },
+  infoText: {
+    fontSize: 10,
+    fontWeight: "400",
+    color: "#8b8b8b",
+  },
+});
 
 export default StatisticsScreen;
