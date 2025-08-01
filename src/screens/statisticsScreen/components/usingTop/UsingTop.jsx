83aae74b0c651a5fbb4b8446608f6d1d3190fa88
Suki <tues1205@naver.com>
2025-08-02 00:36:52 +0900

    ✨ : UsingTop 컴포넌트

---
+++ src/screens/statisticsScreen/components/usingTop/UsingTop.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View } from "react-native";
+import StatisticsBox from "../StatisticsBox";
+import { COLORS } from "../../../../styles/color";
+import UsingTopItem from "./UsingTopItem";
+
+const UsingTop = ({ data }) => {
+  return (
+    <StatisticsBox
+      title="자주 사용하는 문장 TOP 5"
+      height="234"
+      bgColor={COLORS.MAIN_YELLOW2}
+    >
+      <View style={{ flexDirection: "column", gap: 8 }}>
+        {data.slice(0, 5).map((item, i) => (
+          <UsingTopItem key={i} rank={item.rank} text={item.text} />
+        ))}
+      </View>
+    </StatisticsBox>
+  );
+};
+
+export default UsingTop;
---
+++ src/screens/statisticsScreen/components/usingTop/UsingTopItem.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { COLORS } from "../../../../styles/color";
+
+const getBgColorByRank = (rank) => {
+  const opacityMap = {
+    1: 1,
+    2: 0.9,
+    3: 0.8,
+    4: 0.7,
+    5: 0.6,
+  };
+  const opacity = opacityMap[rank] ?? 0.5;
+  return `rgba(255,255,255,${opacity})`;
+};
+
+const UsingTopItem = ({ rank = 1, text = "내용" }) => {
+  const backgroundColor = getBgColorByRank(rank); // 배경색
+
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      {/* 왼쪽: 순위 */}
+      <View style={styles.left}>
+        <Text style={styles.rankText}>{rank}위</Text>
+      </View>
+
+      {/* 가운데 선 */}
+      <View style={styles.divider} />
+
+      {/* 오른쪽: 내용 */}
+      <View style={styles.right}>
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 311,
+    height: 28,
+    backgroundColor: COLORS.WHITE,
+    borderRadius: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  left: {
+    width: 35,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    width: 2,
+    height: "100%",
+    backgroundColor: COLORS.MAIN_YELLOW2,
+  },
+  right: {
+    flex: 1,
+    justifyContent: "center",
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+  rankText: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: COLORS.SUB_BLACK,
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: "400",
+    color: COLORS.SUB_BLACK,
+  },
+});
+
+export default UsingTopItem;
