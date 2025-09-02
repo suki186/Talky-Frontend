dd8d1b82f451f10a39ded22fd9c9229e217b6453
a-neey <2371429@hansung.ac.kr>
2025-09-02 12:06:09 +0900

    ✨ : 센터 위치 거리에 따른 필터링 추가

--- src/screens/mapScreen/MapScreen.jsx
+++ src/screens/mapScreen/MapScreen.jsx
@@ -5,42 +5,31 @@ import Selector from "../../components/Selector";
 import { COLORS } from "../../styles/color";
 
 const MapScreen = () => {
-  const regions = [
+  const distances = [
     "전체",
-    "서울특별시",
-    "인천광역시",
-    "부산광역시",
-    "광주광역시",
-    "대구광역시",
-    "대전광역시",
-    "울산광역시",
-    "경기도",
-    "충청북도",
-    "충청남도",
-    "전라북도",
-    "전라남도",
-    "경상북도",
-    "경상남도",
-    "강원도",
-    "제주특별자치도",
-  ]; // 지역 목록
-  const [region, setRegion] = useState("");
+    "1km 이내",
+    "1km ~ 5km",
+    "5km ~ 7km",
+    "7km ~ 10km",
+    "10km ~ 20km"
+  ]; // 거리 구분
+  const [distance, setDistance] = useState("");
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.selectorContainer}>
         <Selector
           width="112"
-          items={regions}
-          selectedValue={region}
-          onSelect={setRegion}
-          placeholder="지역 선택"
+          items={distances}
+          selectedValue={distance}
+          onSelect={setDistance}
+          placeholder="거리 선택"
         />
       </View>
       <Text style={styles.description}>
         전국 언어치료센터의 위치를 한 눈에 확인할 수 있습니다.
       </Text>
-      <MapBox selectedRegion = { region } />
+      <MapBox selectedRegion = { distance } />
     </ScrollView>
   );
 };
