e5a17ad1cb7a37f80c57917d19324a6e149e8afc
a-neey <2371429@hansung.ac.kr>
2025-09-04 15:56:03 +0900

    💄 : app 폰트 추가

--- App.jsx
+++ App.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RootNavigator from "./src/navigation/RootNavigator";
 import SplashScreen from "./src/screens/SplashScreen";
+import { Text } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
   const [splashDone, setSplashDone] = useState(false);
-  const isLoggedIn = true; // 로그인여부: 테스트용
+  const isLoggedIn = true;
+
+  // Pretendard 폰트 불러오기
+  const [fontsLoaded] = useFonts({
+    Pretendard: require("./assets/fonts/Pretendard-Bold.otf"),
+  });
+
+  // 전역으로 Text에 폰트 적용
+  useEffect(() => {
+    if (fontsLoaded) {
+      const oldRender = Text.render;
+      Text.render = function (...args) {
+        const origin = oldRender.apply(this, args);
+        return React.cloneElement(origin, {
+          style: [{ fontFamily: "Pretendard" }, origin.props.style],
+        });
+      };
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
 
   if (!splashDone) {
     return (
