6952f140291954d09f73f8f601cee675a8db3a16
a-neey <2371429@hansung.ac.kr>
2025-09-06 23:13:27 +0900

    ✨ : loginUserApi 함수 생성

---
+++ src/apis/auth/loginUserApi.js
@@ -0,0 +1,32 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import defaultInstance from "../utils/instance";
+
+const loginUserApi = async (loginId, password) => {
+  try {
+    const response = await defaultInstance.post(`auth/login`, {
+        loginId: loginId,
+        password: password
+    });
+		const { httpStatus, isSuccess, data, message } = response.data;
+
+    if (httpStatus === 200 && isSuccess) {
+      console.log("로그인 성공");
+      await AsyncStorage.setItem("idtoken", data.token);
+
+      return {
+        userId: data.userId,
+        userType: data.userType,
+        username: data.username,
+      };
+
+    } else {
+      console.warn("로그인 실패: ", message);
+      return null;
+    }
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export default loginUserApi;
