bfe97a03de972481b14579b373ae5ab109adcddd
a-neey <2371429@hansung.ac.kr>
2025-09-08 00:50:38 +0900

    🍱 : 폰트 등록

--- App.jsx
+++ App.jsx
@@ -8,9 +8,17 @@ export default function App() {
   const [splashDone, setSplashDone] = useState(false);
   const isLoggedIn = true;
 
-  // Pretendard 폰트 불러오기
+  // 폰트 불러오기
   const [fontsLoaded] = useFonts({
-    Pretendard: require("./assets/fonts/Pretendard-Bold.otf"),
+    PretendardThin: require("./assets/fonts/Pretendard-Thin.otf"), // 100
+    PretendardExtraLight: require("./assets/fonts/Pretendard-ExtraLight.otf"), // 200
+    PretendardLight: require("./assets/fonts/Pretendard-Light.otf"), // 300
+    PretendardRegular: require("./assets/fonts/Pretendard-Regular.otf"), // 400
+    PretendardMedium: require("./assets/fonts/Pretendard-Medium.otf"), // 500
+    PretendardSemiBold: require("./assets/fonts/Pretendard-SemiBold.otf"), // 600
+    PretendardBold: require("./assets/fonts/Pretendard-Bold.otf"), // 700
+    PretendardExtraBold: require("./assets/fonts/Pretendard-ExtraBold.otf"), // 800
+    PretendardBlack: require("./assets/fonts/Pretendard-Black.otf"), // 900
   });
 
   // 전역으로 Text에 폰트 적용
@@ -20,7 +28,7 @@ export default function App() {
       Text.render = function (...args) {
         const origin = oldRender.apply(this, args);
         return React.cloneElement(origin, {
-          style: [{ fontFamily: "Pretendard" }, origin.props.style],
+          style: [{ fontFamily: "PretendardRegular" }, origin.props.style],
         });
       };
     }
