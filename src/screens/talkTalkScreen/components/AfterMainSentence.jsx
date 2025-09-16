35396a33eb41050a20346046fe5e35e3cb698ae6
Suki <tues1205@naver.com>
2025-09-17 01:26:33 +0900

    ✨ : 메인추천, 즐겨찾기 TTS 기능

--- src/screens/talkTalkScreen/components/AfterMainSentence.jsx
+++ src/screens/talkTalkScreen/components/AfterMainSentence.jsx
@@ -1,34 +1,58 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
-import { COLORS } from "../../../styles/color"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { COLORS } from "../../../styles/color";
+import { useTTS } from "../../../hooks/useTTS";
 
 export const AfterMainSentence = ({ onPress, isSelected, text, pressed }) => {
-    return (
-        <TouchableOpacity
-            onPress = { onPress }
-            activeOpacity = { 1 }
-            style = { [styles.button, 
-                {
-                    opacity: pressed !== undefined ? (pressed ? 1 : 0.5) : (isSelected ? 1 : 0.5)
-                }]}
-        >
-            <Text style = { styles.recomText }>{ text }</Text>
-        </TouchableOpacity>
-    )
-}
+  const { speaking, speak, stop } = useTTS({
+    language: "ko",
+    pitch: 1.0,
+    rate: 0.8,
+  });
+
+  // TTS 실행 함수
+  const handlePress = async () => {
+    if (onPress) onPress();
+
+    // 이미 말하고 있으면 -> 다시 재생
+    if (speaking) await stop();
+
+    // TTS 실행
+    speak(text, {
+      onDone: () => console.log("TTS 완료"),
+      onError: (e) => console.warn("TTS 에러:", e),
+    });
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={1}
+      style={[
+        styles.button,
+        {
+          opacity:
+            pressed !== undefined ? (pressed ? 1 : 0.5) : isSelected ? 1 : 0.5,
+        },
+      ]}
+    >
+      <Text style={styles.recomText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    button: {
-        width: 302.67,
-        height: 40,
-        backgroundColor: COLORS.WHITE,
-        borderRadius: 26.67,
-        paddingHorizontal: 13,
-        paddingVertical: 10,
-        justifyContent: "center",
-    },
+  button: {
+    width: 302.67,
+    height: 40,
+    backgroundColor: COLORS.WHITE,
+    borderRadius: 26.67,
+    paddingHorizontal: 13,
+    paddingVertical: 10,
+    justifyContent: "center",
+  },
 
-    recomText: {
-        fontSize: 12,
-        fontFamily: "PretendardMedium",
-    }
-})
+  recomText: {
+    fontSize: 12,
+    fontFamily: "PretendardMedium",
+  },
+});
