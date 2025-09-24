9043b79496c592b6721432e7500d1bb0eccb5ad8
Suki <tues1205@naver.com>
2025-09-24 18:51:30 +0900

    ✨ : 일반사용자 설정 로딩스피너 적용

--- src/screens/userSettingScreen/UserSettingScreen.jsx
+++ src/screens/userSettingScreen/UserSettingScreen.jsx
@@ -4,6 +4,7 @@ import UserInfo from "./components/UserInfo";
 import EmergencyContact from "./components/EmergencyContact";
 import VoiceSetting from "./components/VoiceSetting";
 import LogoutButton from "../../components/auth/LogoutButton";
+import LoadingSpinner from "../../components/LoadingSpinner";
 import { COLORS } from "../../styles/color";
 import getUserInfoApi from "../../apis/userSetting/getUserInfoApi";
 import { buildUserSettingHandlers } from "./userSettingHandlers";
@@ -53,43 +54,49 @@ const UserSettingScreen = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      {/* 사용자 정보 */}
-      <UserInfo
-        userName={user?.username ?? "이름을 입력해 주세요"}
-        loginId={user?.loginId ?? "-"}
-        connectionCode={user?.connectionCode ?? "-"}
-        introduction={
-          user?.introduction ??
-          "저는 언어 표현이 어려운 상황입니다. 양해 부탁드립니다."
-        }
-        onChangeName={handleUserNameChange}
-        onChangeIntro={handleUserIntroChange}
-      />
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          {/* 사용자 정보 */}
+          <UserInfo
+            userName={user?.username ?? "이름을 입력해 주세요"}
+            loginId={user?.loginId ?? "-"}
+            connectionCode={user?.connectionCode ?? "-"}
+            introduction={
+              user?.introduction ??
+              "저는 언어 표현이 어려운 상황입니다. 양해 부탁드립니다."
+            }
+            onChangeName={handleUserNameChange}
+            onChangeIntro={handleUserIntroChange}
+          />
 
-      {/* 긴급 연락처 */}
-      <EmergencyContact
-        selectedTarget={user?.emergencyTarget ?? "119"}
-        onChangeTarget={handleSosChange}
-        initGuardName={
-          user?.guardianInfo?.guardianName ?? "이름을 입력해 주세요"
-        }
-        initGuardPhone={user?.guardianInfo?.guardianPhone ?? "010-0000-0000"}
-      />
+          {/* 긴급 연락처 */}
+          <EmergencyContact
+            selectedTarget={user?.emergencyTarget ?? "119"}
+            onChangeTarget={handleSosChange}
+            initGuardName={
+              user?.guardianInfo?.guardianName ?? "이름을 입력해 주세요"
+            }
+            initGuardPhone={
+              user?.guardianInfo?.guardianPhone ?? "010-0000-0000"
+            }
+          />
 
-      {/* 음성 설정 */}
-      <VoiceSetting
-        ttsSettings={user?.ttsSettings || settings}
-        onSave={(newSettings) => {
-          handleTtsChange(newSettings); // DB 저장
-          setUser((prev) => ({
-            ...prev,
-            ttsSettings: newSettings,
-          }));
-          setSettings(newSettings); // Context 갱신
-        }}
-      />
-
-
+          {/* 음성 설정 */}
+          <VoiceSetting
+            ttsSettings={user?.ttsSettings || settings}
+            onSave={(newSettings) => {
+              handleTtsChange(newSettings); // DB 저장
+              setUser((prev) => ({
+                ...prev,
+                ttsSettings: newSettings,
+              }));
+              setSettings(newSettings); // Context 갱신
+            }}
+          />
+        </>
+      )}
       {/* 로그아웃 버튼 */}
       <LogoutButton />
     </ScrollView>
@@ -98,7 +105,7 @@ const UserSettingScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 33,
+    paddingTop: 20,
     paddingBottom: 18,
     backgroundColor: COLORS.BACKGROUND,
     alignItems: "center",
